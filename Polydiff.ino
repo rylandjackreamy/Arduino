4a1553f4eed4b4848a71c077285dfcf3b5a0f096
timmy17 <t.gentry1111@gmail.com>
2015-04-03 17:36:19 -0700

    Update Polydiff.ino

--- Polydiff.ino
+++ Polydiff.ino
@@ -1,53 +1,48 @@
 
-/*
-String readd() {
-  String rett = ""; // again ret is a return variable
-  char in; // char is the character input
-  while(!Serial.available()){}
-  while (Serial.available() > 0){ // loop strings together the input
-    Serial.println(Serial.available());
-    in = Serial.read();
-    Serial.println(in);
-    rett += in; // appends the input character to the return string
-  }
-  Serial.println(rett);
-  return rett;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
-*/
+
+String readd() {
+  while(Serial.available() <= 0){}
+  String in = Serial.readString();
+  Serial.println(in);
+  return in;
+}
+
 
 String diff() {
-  int i = 0, j;
+  int i = -1, j;
   String ret = "";
   int place = 0;
   String working = "", coefficient, power, ending = "", last = "";
-  String input = "3x^34+4x-5"; //readd();
-  Serial.println(input);
-  while(i < input.length()) {
+  String input = readd();
+  do {
+    i ++;
     coefficient = "";
     power = "";
     if (input.charAt(i) != '+' && input.charAt(i) != '-' && i < input.length()){
       working += input.charAt(i);
     } else {
-      Serial.println(working);
       j = 0;
-      ret = "";
       coefficient = "";
       power = "";
+      if (working.charAt(j) == 'x'){
+        coefficient = "1";
+      }
       while(working.charAt(j) != 'x' && j < working.length()) {
         coefficient += working.charAt(j);
         j++;
       }
-      Serial.println(coefficient);
       if (working.charAt(j) == 'x'){
-        Serial.println(working.charAt(j));
         if (working.charAt(j + 1) == '^') {
-          Serial.println(working.charAt(j + 1));
           j += 2;
           while (j < working.length()){
             power += working.charAt(j);
             j++;
           }
-          Serial.println(power);
           int coef = coefficient.toInt();
           int powow = power.toInt();
           coef = coef * powow;
@@ -57,32 +52,34 @@ String diff() {
             ret += String(coef);
             ret += "x^";
             ret += String(powow);
+            working = "";
           } else {
             ret += last;
             ret += String(coef);
             ret += "x";
-          }
-          if (input.charAt(i) == '+' || input.charAt(i) == '-'){
-            last = String(input.charAt(i));
-          } else {
-            last = "";
+            working = "";
           }
         } else {
           int coef = coefficient.toInt();
-          ret += String(coef) + ending;
+          ret += last + String(coef);
+          working = "";
         }
+        if (input.charAt(i) == '+' || input.charAt(i) == '-'){
+          last = String(input.charAt(i));
+        } else {
+          last = "";
+        }
+      } else {
+        working = "";
       }
     }
-    i ++;
-  }
+  } while(i < input.length());
   return ret;
 }
 
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+
+
+void loop(){
   Serial.println(diff());
 }
-
-void loop(){}
